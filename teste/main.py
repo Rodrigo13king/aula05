7b2f998fe9ba45b6399683baa894e413257f2699
rodrigosouza <mc2016.rodrigo@gmail.com>
2025-04-28 13:57:33 -0300

    first commit

---
+++ teste/main.py
@@ -0,0 +1,8 @@
+nome = input("digite seu nome :")
+idade = int("digite sua idade :")
+salario = float(input("digite seu salario :"))
+porcentagem = float ("digite o valor do salario :")
+soma = float ("some o valor do salario + 20 porcento : =")
+novosalario = float("reajuste * salario / 100) + salario")
+
+print(f"({nome}  {idade}  {salario} {novosalario})")
